29c8269fbb766ecee41d5f4ef9e5747ae8c87ee4
WASEEM ATTAR <waseem_6957@yahoo.com>
2020-12-15 10:57:38 +0530

    Add files via upload

--- app/src/main/java/com/infostackresearch/homefit/http/APIService.java
+++ app/src/main/java/com/infostackresearch/homefit/http/APIService.java
@@ -7,6 +7,8 @@ import com.infostackresearch.homefit.models.PaymentHash;
 import com.infostackresearch.homefit.models.PlanResponse;
 import com.infostackresearch.homefit.models.ProfileSubscription;
 import com.infostackresearch.homefit.models.SignUp;
+import com.infostackresearch.homefit.models.SubscribeUser;
+import com.infostackresearch.homefit.models.SubscriptionData;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -36,4 +38,127 @@ public interface APIService {
     @POST("get-hash")
     Call<PaymentHash> getHash(@Body PayData payData);
 
+    @Headers("Content-Type: application/json")
+    @POST("customer/user-subscribe")
+    Call<SubscribeUser> doSubscription(@Body SubscriptionData subscriptionData, @Header("Authorization") String auth_token);
+
+//    @FormUrlEncoded
+//    @POST("admin-login.php")
+//    Call<AdminModel> getAdminData(@Field("emp_code") String user_id);
+//
+//    @FormUrlEncoded
+//    @POST("new-mpin.php")
+//    Call<NewMPin> setMPin(@Field("emp_code") String user_id, @Field("mpin") String mpin);
+//
+//    @FormUrlEncoded
+//    @POST("apply-leave.php")
+//    Call<LeaveModel> applyLeave(@Field("emp_code") String emp_code,
+//                                @Field("account_id") String account_id,
+//                                @Field("joining_date") String joining_date,
+//                                @Field("from_date") String from_date,
+//                                @Field("to_date") String to_date,
+//                                @Field("leave_id") String leave_id,
+//                                @Field("leave_reason") String leave_reason,
+//                                @Field("leave_day") String leave_day);
+//
+//    @FormUrlEncoded
+//    @POST("leave-status.php")
+//    Call<LeaveModel> leaveStatus(@Field("emp_code") String emp_code);
+//
+//    @FormUrlEncoded
+//    @POST("leave-balance.php")
+//    Call<LeaveModel> leaveBalance(@Field("emp_code") String emp_code, @Field("leave_id") String leave_id);
+//
+//    @FormUrlEncoded
+//    @POST("leave-report.php")
+//    Call<LeaveModel> leaveReport(@Field("emp_code") String emp_code);
+//
+//    @Multipart
+//    @POST("mark-attendance.php")
+//    Call<Punch> mark_attendance(
+//            @Part("emp_code") RequestBody emp_code,
+//            @Part("latitude") RequestBody latitude,
+//            @Part("longitude") RequestBody longitude,
+//            @Part("address") RequestBody address,
+//            @Part MultipartBody.Part file
+//    );
+//
+////    @FormUrlEncoded
+////    @POST("/attendance-report.php")
+////    Call<AttendanceHistory> getAttendanceReport(@Field("emp_code") String emp_code,
+////                                                @Field("account_id") String account_id,
+////                                                @Field("month")  String month,
+////                                                @Field("monthtext") String month_text,
+////                                                @Field("year") String year
+////    );
+//
+//    @FormUrlEncoded
+//    @POST("attendance-total-report.php")
+//    Call<AttendanceHistory> getAttendanceReport(@Field("emp_code") String emp_code,
+//                                                @Field("account_id") String account_id,
+//                                                @Field("month") String month,
+//                                                @Field("monthtext") String month_text,
+//                                                @Field("year") String year
+//    );
+//
+//    @FormUrlEncoded
+//    @POST("get-punch.php")
+//    Call<PunchDetails> getPunchDetails(@Field("emp_code") String emp_code);
+//
+//    @FormUrlEncoded
+//    @POST("attendance-details.php")
+//    Call<AttendanceDetails> getAttedanceDetails(@Field("emp_code") String emp_code,
+//                                                @Field("unique_id") String unique_id);
+//
+////    @FormUrlEncoded
+////    @POST("salary-report.php")
+////    Call<SalaryReport> getSalaryDetails(@Field("month") String month, @Field("monthtext") String monthtext, @Field("year") String year, @Field("emp_code") String emp_code,@Field("account_id") String account_id);
+//
+//    @FormUrlEncoded
+//    @POST("salary-report-new.php")
+//    Call<SalaryReport> getSalaryDetails(@Field("salary_month") String month, @Field("emp_code") String emp_code);
+//
+//    @FormUrlEncoded
+//    @POST("leave-master.php")
+//    Call<LeaveMasterFetch> getLeaveMaster(@Field("emp_code") String emp_code, @Field("account_id") String account_id);
+//
+//    @FormUrlEncoded
+//    @POST("login-status.php")
+//    Call<LoginStatus> setLoginStatus(@Field("emp_code") String emp_code, @Field("login_status") int login_status);
+//
+//    @FormUrlEncoded
+//    @POST("admin-loginstatus.php")
+//    Call<LoginStatus> setAdminLoginStatus(@Field("emp_code") String emp_code, @Field("login_status") int login_status);
+//
+//    @FormUrlEncoded
+//    @POST("admin-fingerdata.php")
+//    Call<FingerDetails> getFingerData(@Field("emp_code") String emp_code, @Field("finger_name") String finger_name);
+//
+//    @FormUrlEncoded
+//    @POST("admin-markattendance.php")
+//    Call<Punch> mark_admin_attendance(@Field("emp_code") String emp_code, @Field("latitude") String latitude, @Field("longitude") String longitude, @Field("address") String address);
+//
+//    @FormUrlEncoded
+//    @POST("admin-employees.php")
+//    Call<AdminEmployeesModel> getAdminEmployees(@Field("emp_code") String emp_code);
+//
+//    @FormUrlEncoded
+//    @POST("sendotp.php")
+//    Call<SendOTPModel> sendOTP(@Field("emp_code") String emp_code);
+//
+//    @FormUrlEncoded
+//    @POST("verifyotp.php")
+//    Call<VerifyOTPModel> verifyOTP(@Field("emp_code") String emp_code, @Field("otp") String otp);
+//
+//    @FormUrlEncoded
+//    @POST("admin-sendotp.php")
+//    Call<SendOTPModel> sendAdminOTP(@Field("emp_code") String emp_code);
+//
+//    @FormUrlEncoded
+//    @POST("admin-verifyotp.php")
+//    Call<VerifyOTPModel> verifyAdminOTP(@Field("emp_code") String emp_code, @Field("otp") String otp);
+//
+//    @FormUrlEncoded
+//    @POST("admin-newmpin.php")
+//    Call<NewMPin> setAdminMPin(@Field("emp_code") String user_id, @Field("mpin") String mpin);
 }
